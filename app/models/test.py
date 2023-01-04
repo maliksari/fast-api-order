139c7b0efbb305e79f6afeddd74d24d7c28701fc
maliksari <malik.sari06@gmail.com>
2023-01-04 21:07:33 +0300

    deleted migrations

--- app/models/test.py
+++ app/models/test.py
@@ -3,6 +3,7 @@ from sqlmodel import Field
 from .base import BaseModel
 
 
-class Test(BaseModel,table=True):
+class Test(BaseModel, table=True):
     name: str = Field(max_length=150, nullable=False)
-    test:str 
+    test: str
+    test2: str = Field(max_length=150, nullable=False)
